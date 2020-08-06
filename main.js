ea2e2f1c36f11b39beade4b9831d1c391627d2f4
Maks Iwanek <iwanekmaksymilian@gmail.com>
2020-08-06 21:08:34 +0200

    More buttons working all gitowa

--- main.js
+++ main.js
@@ -5,63 +5,118 @@ const szukaj = document.querySelector(".szukaj");
 const szukajBox = document.querySelector(".szukajBox");
 const butonEurope = document.querySelector(".btnEurope");
 const krajeEuropy = document.querySelector(".krajeEuropy");
+const buttonAmeryki = document.querySelector(".btnAmerican");
+const buttonAzji = document.querySelector(".btnAsian");
+const buttonAfryki = document.querySelector(".btnAfrica");
+const krajeAmeryki = document.querySelector(".krajeAmeryki");
+const krajeAfryki = document.querySelector(".krajeAfryki")
+const krajeAzji = document.querySelector(".krajeAzji");
+buttonAmeryki.addEventListener("click", americaCountries);
+buttonAzji.addEventListener("click", asiaCountries);
+buttonAfryki.addEventListener("click", africaCountries);
 btn.addEventListener("click", allCountries);
 butonEurope.addEventListener("click", europeCountries);
     let output = "";
     let europeOutput = "";
+    let americaOutput = "";
+    let africaOutput = "";
+    let asiaOutput = "";
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
-    let howmany = 0;
     xhr.addEventListener("load", function () {
         if (xhr.status === 200) {
             for (const el of xhr.response) {
-                output += `<div class="bigBox"><img src="${el.flag}"><p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}</p></div>`; //region europe
-                if (el.region == "Europe") {
-                    europeOutput += `<div class="bigBox"><img src="${el.flag}"><p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}</p></div>`;
+                output += `
+                <div class="bigBox"><img src="${el.flag}"><p class="panstwo">
+                ${el.name}</p> <p class="stolica">${el.capital}</p></div>`; //region europe
+                if (el.region == "Europe")
+                {
+                    europeOutput += `<div class="bigBox"><img src="${el.flag}">
+                    <p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}
+                    </p>
+                    </div>`;
                 }
+                else if (el.region == "Americas")
+                {
+                    americaOutput += `<div class="bigBox"><img src="${el.flag}">
+                    <p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}
+                    </p>
+                    </div>`;
+                }
+                else if (el.region == "Africa") {
+                    africaOutput += `<div class="bigBox"><img src="${el.flag}">
+                    <p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}
+                    </p>
+                    </div>`;
+                }
+                else if (el.region == "Asia") {
+                    asiaOutput += `<div class="bigBox"><img src="${el.flag}">
+                    <p class="panstwo">${el.name}</p> <p class="stolica">${el.capital}
+                    </p>
+                    </div>`;
+                }
+
             }
             kraje.innerHTML = output;
             krajeEuropy.innerHTML = europeOutput;
+            krajeAmeryki.innerHTML = americaOutput;
+            krajeAfryki.innerHTML = africaOutput;
+            krajeAzji.innerHTML = asiaOutput;
         }
 
     });
     xhr.open("GET", "https://restcountries.eu/rest/v2", true);
     xhr.send();
 
-//Wszystkie kraje Świata
-function allCountries() {
-    if (krajeEuropy.style.display == "grid") {
-        krajeEuropy.style.display = "none";
+//searching a country typed in input
+function wyszukaj () {
+    let panstwa = document.querySelectorAll(".panstwo");
+    let value = this.value;
+    let tab = value.split(" ");
+    if (tab != "") {
+        for (let i = 0; i < tab.length; i++) {
+            tab[i] = tab[i][0].toUpperCase() + tab[i].substr(1);
+        }
     }
-    kraje.style.display = "grid";
-    szukaj.style.display = "unset";
-    szukaj.addEventListener("input", function () {
-        let panstwa =  document.querySelectorAll(".panstwo");
-        let value = this.value;
-            let tab = value.split(" ");
-            if (tab != "") {
-                for (let i = 0; i < tab.length; i++) {
-                        tab[i] = tab[i][0].toUpperCase() + tab[i].substr(1);
-                }
-            }
-            let szukane = tab.join(" ");
-            //console.log(szukane);
-            panstwa.forEach(function (el) {
-                let text = el.innerText;
-                if (text.search(szukane) != -1) {
-                    el.closest(".bigBox").style.setProperty("display", "");
-                }
-                else {
-                    el.closest(".bigBox").style.setProperty("display", "none");
-                }
-            })
-        //}
+    let szukane = tab.join(" ");
+    panstwa.forEach(function (el) {
+        let text = el.innerText;
+        if (text.search(szukane) != -1) {
+            el.closest(".bigBox").style.setProperty("display", "");
+        }
+        else {
+            el.closest(".bigBox").style.setProperty("display", "none");
+        }
     })
+}
+szukaj.addEventListener("input", wyszukaj);
+function przewin() {
     window.scroll({
-        top: 980,
+        top: 970,
         left: 100,
         behavior: 'smooth'
     });
+}
+
+
+/**
+*  Wszystkie kraje Świata
+**/
+
+function allCountries() {
+    if (krajeEuropy.style.display == "grid" || krajeAfryki.style.display == "grid" ||
+        krajeAzji.style.display == "grid"   || krajeAmeryki.style.display == "grid")
+    {
+        krajeEuropy.style.display = "none";
+        krajeAzji.style.display = "none";
+        krajeAfryki.style.display = "none";
+        krajeAmeryki.style.display = "none";
+    }
+    kraje.style.display = "grid";
+    szukaj.style.display = "unset";
+    przewin();
+
+
 };
 
 /**
@@ -69,41 +124,59 @@ function allCountries() {
 **/
 
 function europeCountries() {
-    szukaj.style.display = "unset";
-    kraje.style.display = "none";
-    szukaj.addEventListener("input", function () {
-        let panstwa = document.querySelectorAll(".panstwo");
-        let value = this.value;
-        window.scroll({
-            top: 970,
-            left: 100,
-            behavior: 'smooth'
-        });
-        let tab = value.split(" ");
-        if (tab != "") {
-            for (let i = 0; i < tab.length; i++) {
-                tab[i] = tab[i][0].toUpperCase() + tab[i].substr(1);
-            }
-        }
-        let szukane = tab.join(" ");
-        //console.log(szukane);
-        panstwa.forEach(function (el) {
-            let text = el.innerText;
-            if (text.search(szukane) != -1) {
-                el.closest(".bigBox").style.setProperty("display", "");
-            }
-            else {
-                el.closest(".bigBox").style.setProperty("display", "none");
-            }
-        })
-        //}
-    })
+    if (krajeAfryki.style.display == "grid" || krajeAzji.style.display == "grid" || krajeAmeryki.style.display == "grid" || kraje.style.display == "grid") {
+        krajeAzji.style.display = "none";
+        krajeAfryki.style.display = "none";
+        krajeAmeryki.style.display = "none";
+        kraje.style.display = "none";
+    }
     krajeEuropy.style.display = "grid";
-    window.scroll({
-        top: 970,
-        left: 100,
-        behavior: 'smooth'
-    });
-
+    szukaj.style.display = "unset";
+    przewin();
+}
+/**
+*  Kraje Ameryki
+**/
+function americaCountries() {
+    if (krajeEuropy.style.display == "grid" || krajeAfryki.style.display == "grid" ||
+        krajeAzji.style.display == "grid" || kraje.style.display == "grid")
+    {
+        krajeEuropy.style.display = "none";
+        krajeAzji.style.display = "none";
+        krajeAfryki.style.display = "none";
+        kraje.style.display = "none";
+    }
+    szukaj.style.display = "unset";
+    krajeAmeryki.style.display = "grid";
+    przewin();
+}
+/**
+*  Kraje Azji
+**/
+function asiaCountries() {
+    if (krajeEuropy.style.display == "grid" || krajeAfryki.style.display == "grid" || krajeAmeryki.style.display == "grid" || kraje.style.display == "grid") {
+        krajeEuropy.style.display = "none";
+        krajeAfryki.style.display = "none";
+        krajeAmeryki.style.display = "none";
+        kraje.style.display = "none";
+    }
+    szukaj.style.display = "unset";
+    krajeAzji.style.display = "grid";
+    przewin();
 }
 
+
+/**
+*  Kraje Afryki
+**/
+function africaCountries() {
+    if (krajeEuropy.style.display == "grid" || krajeAzji.style.display == "grid" || krajeAmeryki.style.display == "grid" || kraje.style.display == "grid") {
+        krajeEuropy.style.display = "none";
+        krajeAzji.style.display = "none";
+        krajeAmeryki.style.display = "none";
+        kraje.style.display = "none";
+    }
+    krajeAfryki.style.display = "grid";
+    szukaj.style.display = "unset";
+    przewin();
+}
